54dbf9ca037c0e0fa86aaf3a52e3a2ab9de7528e
arnold-1324 <141809292+arnold-1324@users.noreply.github.com>
2024-03-23 12:54:14 +0530

    2nd part

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 
+import Home from "./pages/Home";
 function App() {
   
   return (
-    <>
-  
-    </>
+   <div className="p-4 h-screen flex items-center justify-center">
+
+    <Home />
+   </div>
   )
 }
 

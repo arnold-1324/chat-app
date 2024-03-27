a2632fbfd3117a35d1505d6435a0bae3d7fd7005
THAVASEE <m.thavasi6040gmail.com>
2024-03-27 10:17:24 +0530

    last commit

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,16 +1,14 @@
 import User from "../models/user.model.js";
-export const getUserForsidebar = async(req,res)=>{
-    try {
 
-        const loggedInUser = req.user._id;
+export const getUsersForSidebar = async (req, res) => {
+	try {
+		
 
-        const alluser = await User.find().select("-password");
+		const AllUsers = await User.find().select("-password");
 
-        res.status(200).json(alluser);
-        
-    } catch (error) {
-        console.log("Error in the getUserForsidebar controller",error.message);
-        res.status(500).json({error:"Internal server error"})
-        
-    }
-}
+		res.status(200).json(AllUsers);
+	} catch (error) {
+		console.error("Error in getUsersForSidebar: ", error.message);
+		res.status(500).json({ error: "Internal server error" });
+	}
+};
--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,17 +1,14 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 export const AuthContext = createContext();
 
-export const useAuthContext = ()=>{
-    return useContext(AuthContext);
-}
+// eslint-disable-next-line react-refresh/only-export-components
+export const useAuthContext = () => {
+	return useContext(AuthContext);
+};
 
-export const AuthContextProvider= ({ children })=>{
+export const AuthContextProvider = ({ children }) => {
+	const [authUser, setAuthUser] = useState(JSON.parse(localStorage.getItem("chat-user")) || null);
 
-    const [authUser, setAuthUser ]= useState(JSON.parse(localStorage.getItem("chat-user")) || null);
-
-
-    return <AuthContextProvider value={{authUser, setAuthUser}}>
-        {children}
-    </AuthContextProvider>
-}
+	return <AuthContext.Provider value={{ authUser, setAuthUser }}>{children}</AuthContext.Provider>;
+};

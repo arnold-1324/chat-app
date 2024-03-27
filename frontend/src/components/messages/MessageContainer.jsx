4eba7e44801f9bf33dc92d65d2bc9baf09c93306
THAVASEE <m.thavasi6040gmail.com>
2024-03-27 11:10:12 +0530

    dep

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -14,13 +14,13 @@ const MessageContainer = () => {
 	}, [setSelectedConversation]);
 
 	return (
-		<div className='md:min-w-[550px] flex flex-col'>
+		<div className='md:min-w-[450px] flex flex-col'>
 			{!selectedConversation ? (
 				<NoChatSelected />
 			) : (
 				<>
 					{/* Header */}
-					<div className='bg-rose-300 px-4 py-2 mb-2'>
+					<div className='bg-slate-500 px-4 py-2 mb-2'>
 						<span className='label-text'>To:</span>{" "}
 						<span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
 					</div>
@@ -46,3 +46,23 @@ const NoChatSelected = () => {
 	);
 };
 
+// STARTER CODE SNIPPET
+// import MessageInput from "./MessageInput";
+// import Messages from "./Messages";
+
+// const MessageContainer = () => {
+// 	return (
+// 		<div className='md:min-w-[450px] flex flex-col'>
+// 			<>
+// 				{/* Header */}
+// 				<div className='bg-slate-500 px-4 py-2 mb-2'>
+// 					<span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>John doe</span>
+// 				</div>
+
+// 				<Messages />
+// 				<MessageInput />
+// 			</>
+// 		</div>
+// 	);
+// };
+// export default MessageContainer;
